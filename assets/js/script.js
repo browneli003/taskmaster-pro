3480ef5f4c212e405b14474812865d5d2da4482b
Elia Brown <eliabrown@Elias-MacBook.local>
2021-07-17 20:00:20 -0600

    fonts

--- assets/js/script.js
+++ assets/js/script.js
@@ -280,5 +280,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
+
 // load tasks for the first time
 loadTasks();
